4fd0fed8c34d26924f268b85b78db225a5f354be
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-11-08 10:22:31 -0500

    Add grid divs via dom to #container

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const container = document.querySelector("#container");
+
+for (let i = 0; i < 16; i++) {
+  const gridDiv = document.createElement("div");
+  gridDiv.classList.add("gridDiv");
+  container.appendChild(gridDiv);
+}
