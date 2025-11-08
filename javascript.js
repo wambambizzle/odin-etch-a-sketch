e4238eb256870a8e8043ecbe4b04e48120afe61f
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-11-08 10:43:36 -0500

    Rename the grid div class to grid

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ const container = document.querySelector("#container");
 
 // update later: 256
 for (let i = 0; i < 16; i++) {
-  const gridDiv = document.createElement("div");
-  gridDiv.classList.add("gridDiv");
-  container.appendChild(gridDiv);
+  const grid = document.createElement("div");
+  grid.classList.add("grid");
+  container.appendChild(grid);
 }
