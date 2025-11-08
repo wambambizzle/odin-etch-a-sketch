d9f7249d31d30932f78d3d1de42c7852e08b439c
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-11-08 10:53:11 -0500

    Add a basic hover effect to the grid divs

--- javascript.js
+++ javascript.js
@@ -4,5 +4,14 @@ const container = document.querySelector("#container");
 for (let i = 0; i < 16; i++) {
   const grid = document.createElement("div");
   grid.classList.add("grid");
+
+  grid.addEventListener("mouseenter", () => {
+    grid.classList.add("hover-effect");
+  });
+
+  grid.addEventListener("mouseleave", () => {
+    grid.classList.remove("hover-effect");
+  });
+
   container.appendChild(grid);
 }
