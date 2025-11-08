8778164559a64de5dbcb25e5e1ef260dc6c0ec51
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-11-08 10:42:50 -0500

    Add styling to show the grid of divs

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 const container = document.querySelector("#container");
 
+// update later: 256
 for (let i = 0; i < 16; i++) {
   const gridDiv = document.createElement("div");
   gridDiv.classList.add("gridDiv");
