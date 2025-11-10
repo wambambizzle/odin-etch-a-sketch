b07095fe093124f48808ce7dc6b5902a570a2bb7
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-11-10 15:50:55 -0500

    Refactor the grid setting to a func - clear out previous grid children when setting custom number - clean up css

--- javascript.js
+++ javascript.js
@@ -2,21 +2,35 @@ const container = document.querySelector("#container");
 const gridContainer = document.querySelector(".grid-container");
 const gridSizeButton = document.querySelector("#grid-size-button");
 
-for (let i = 0; i < 64; i++) {
-  const grid = document.createElement("div");
-  grid.classList.add("grid");
-
-  grid.addEventListener("mouseenter", () => {
-    grid.classList.add("hover-effect");
-  });
-
-  grid.addEventListener("mouseleave", () => {
-    grid.classList.remove("hover-effect");
-  });
-
-  gridContainer.appendChild(grid);
-}
-
 gridSizeButton.addEventListener("click", () => {
   let result = prompt("What size grid would you like? (From 2-100)");
+  setGrid(Number(result));
 });
+
+function setGrid(itemsPerRow = 4) {
+  // Reset the grid when settings custom size
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+
+  // Square the number
+  const numberOfItems = itemsPerRow * itemsPerRow;
+
+  // Set the grid
+  for (let i = 0; i < numberOfItems; i++) {
+    const grid = document.createElement("div");
+    grid.classList.add("grid");
+
+    grid.addEventListener("mouseenter", () => {
+      grid.classList.add("hover-effect");
+    });
+
+    grid.addEventListener("mouseleave", () => {
+      grid.classList.remove("hover-effect");
+    });
+
+    gridContainer.appendChild(grid);
+  }
+}
+
+setGrid();
