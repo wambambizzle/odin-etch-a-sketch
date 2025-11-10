5d54ecbb84a6b14c0dcd8a8c7b44ba1eecef0dea
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-11-10 18:26:02 -0500

    Change hover colors to be randomized - add a clear button to remove all highlighted colors

--- javascript.js
+++ javascript.js
@@ -1,10 +1,12 @@
 const container = document.querySelector("#container");
 const gridContainer = document.querySelector(".grid-container");
 const gridSizeButton = document.querySelector("#grid-size-button");
+const clearButton = document.querySelector(".clear-button");
 const gridWidth = 600;
 const gridHeight = 600;
 
 gridSizeButton.addEventListener("click", promptForGridSize);
+clearButton.addEventListener("click", clearGridColors);
 
 setGrid();
 
@@ -18,18 +20,13 @@ function setGrid(itemsPerRow = 4) {
   // Fill the grid
   for (let i = 0; i < numberOfItems; i++) {
     const grid = document.createElement("div");
-
-    grid.style.width = widthAndHeight;
+    grid.className = grid.style.width = widthAndHeight;
     grid.style.height = widthAndHeight;
     grid.style.border = "2px solid black";
     grid.style.boxSizing = "border-box";
 
     grid.addEventListener("mouseenter", () => {
-      grid.classList.add("hover-effect");
-    });
-
-    grid.addEventListener("mouseleave", () => {
-      grid.classList.remove("hover-effect");
+      grid.style.background = generateRandomColor();
     });
 
     gridContainer.appendChild(grid);
@@ -44,4 +41,15 @@ function promptForGridSize() {
   }
 
   setGrid(result);
+}
+
+function clearGridColors() {
+  gridContainer.childNodes.forEach((node) => {
+    node.style.background = "white";
+  });
+}
+
+function generateRandomColor() {
+  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  return `#${randomColor}`;
 }
