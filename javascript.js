948a700c2e5469b593853c6e77ca0e4af4034161
Jordan Anderson <1735405+wambambizzle@users.noreply.github.com>
2025-11-10 17:26:16 -0500

    Fix calculating the size of the grid items - allows the grid to fill the size proportionally

--- javascript.js
+++ javascript.js
@@ -1,25 +1,32 @@
 const container = document.querySelector("#container");
 const gridContainer = document.querySelector(".grid-container");
 const gridSizeButton = document.querySelector("#grid-size-button");
+const gridWidth = 600;
+const gridHeight = 600;
 
 gridSizeButton.addEventListener("click", () => {
   let result = prompt("What size grid would you like? (From 2-100)");
+  // TODO: add the limits greater than or equal to 2 - less than or equal to 100
   setGrid(Number(result));
 });
 
-function setGrid(itemsPerRow = 4) {
-  // Reset the grid when settings custom size
-  while (gridContainer.firstChild) {
-    gridContainer.removeChild(gridContainer.firstChild);
-  }
+setGrid();
 
-  // Square the number
+function setGrid(itemsPerRow = 8) {
+  gridContainer.innerHTML = "";
+
+  // Total number of items
   const numberOfItems = itemsPerRow * itemsPerRow;
+  const widthAndHeight = gridWidth / itemsPerRow + "px";
 
-  // Set the grid
+  // Fill the grid
   for (let i = 0; i < numberOfItems; i++) {
     const grid = document.createElement("div");
-    grid.classList.add("grid");
+
+    grid.style.width = widthAndHeight;
+    grid.style.height = widthAndHeight;
+    grid.style.border = "2px solid black";
+    grid.style.boxSizing = "border-box";
 
     grid.addEventListener("mouseenter", () => {
       grid.classList.add("hover-effect");
@@ -32,5 +39,3 @@ function setGrid(itemsPerRow = 4) {
     gridContainer.appendChild(grid);
   }
 }
-
-setGrid();
